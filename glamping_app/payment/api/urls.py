d768b3d3aea3e9c0a8aa7065a2b590dbb512844b
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2024-09-05 23:54:11 +0100

    removed payment

--- glamping_app/payment/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from payment.api import views
-
-app_name = "payment"
-
-urlpatterns = [
-    # path("create_payment/", views.PaymentView.as_view(), name="create_payment"),
-    path("create_payment/<int:cart_id>/", views.PaymentView.as_view(), name="create_payment"),
-] 
