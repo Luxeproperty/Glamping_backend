5b0fe235827d65d847ba09cbd0bc08e72b20d7e2
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2024-09-06 00:02:11 +0100

    changed py version

--- glamping_app/booking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from booking.models import *
-
-# Register your models here.
-
-admin.site.register(GuestDetails)
-admin.site.register(Booking)
--- glamping_app/payment/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from booking.models import Booking
-# Create your models here.
-
-class Payment(models.Model):
-    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
-    stripe_charge_id = models.CharField(max_length=255)
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
-    paid_at = models.DateTimeField(auto_now_add=True)
