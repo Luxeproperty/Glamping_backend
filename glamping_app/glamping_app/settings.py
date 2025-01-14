3da01c660b609f4384e8c8accdbccc05a6cdcb81
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2025-01-14 10:15:32 +0000

    moved secret keys

--- glamping_app/glamping_app/settings.py
+++ glamping_app/glamping_app/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4gwjc_0*af0s_r1-6^glauo4-9%r#0+_mtqcf4k(pz&qbh9@(q"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -153,12 +155,9 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "website@countryglampinguk.com"
-EMAIL_HOST_PASSWORD = "CountryGlamping2025"
-# EMAIL_HOST_USER = "eldridgeriberio@gmail.com"
-# EMAIL_HOST_PASSWORD = "adlt udlq kned jyol"
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 DEFAULT_FROM_EMAIL = "website@countryglampinguk.com"
-# DEFAULT_FROM_EMAIL = "eldridgeriberio@gmail.com"
-NOTIFY_EMAIL = "eldridgeriberio@gmail.com"
+NOTIFY_EMAIL = "enquiries@countryglampinguk.com"
 
 STRIPE_PUBLIC_KEY = 'your-stripe-public-key'
 STRIPE_SECRET_KEY = 'your-stripe-secret-key'
